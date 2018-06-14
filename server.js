15580279a8c4d6bbe112c7a25cf283dfcb1a7102
simiogunleye <cimi@Simis-MacBook-Pro.local>
2018-06-13 20:28:28 -0700

    added runServer and closeServer

--- server.js
+++ server.js
@@ -3,9 +3,60 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/', (req,res) => {
-	return res.status(200).send('Success')
+	return res.status(200).render('index');
 });
 
-app.listen(process.env.PORT || 8080);
+
+
+// both runServer and closeServer need to access the same
+// server object, so we declare `server` here, and then when
+// runServer runs, it assigns a value.
+let server;
+
+function runServer() {
+  const port = process.env.PORT || 8080;
+  return new Promise((resolve, reject) => {
+    server = app.listen(port, () => {
+      console.log(`Your app is listening on port ${port}`);
+      resolve(server);
+    }).on('error', err => {
+      reject(err)
+    });
+  });
+}
+
+// like `runServer`, this function also needs to return a promise.
+// `server.close` does not return a promise on its own, so we manually
+// create one.
+function closeServer() {
+  return new Promise((resolve, reject) => {
+    console.log('Closing server');
+    server.close(err => {
+      if (err) {
+        reject(err);
+        // so we don't also call `resolve()`
+        return;
+      }
+      resolve();
+    });
+  });
+}
+
+// if server.js is called directly (aka, with `node server.js`), this block
+// runs. but we also export the runServer command so other code (for instance, test code) can start the server as needed.
+if (require.main === module) {
+  runServer().catch(err => console.error(err));
+};
+
+module.exports = {app, runServer, closeServer};
+
+
+
+
+
+
+
+
+//app.listen(process.env.PORT || 8080);
  
-module.exports = app;
+
--- test/test-server.js
+++ test/test-server.js
@@ -1,30 +1,23 @@
 const chai = require('chai');
 const chaiHttp = require('chai-http');
 chai.use(chaiHttp);
+const {app, runServer, closeServer} = require('../server');
 const expect = chai.expect;
-const server = require('../server');
-
-
 
 describe('GET', function() {
-	it('should return html and a status of 200', function() {
-
-		it('fails, as expected', function(done) { // <= Pass in done callback
-		  chai.request('http://localhost:8080')
-		  .get('/')
-		  .end(function(err, res) {
-		    expect(res).to.have.status(123);
-		    done();                               // <= Call done to signal callback end
-		  });
-		});
-
-		it('succeeds silently!', function() {   // <= No done callback
-		  chai.request('http://localhost:8080')
-		  .get('/')
-		  .end(function(err, res) {
-		    expect(res).to.have.status(200);    // <= Test completes before this runs
-		  });
-		});	
+	before(function() {
+		return runServer();
 	});
+	after(function() {
+		return closeServer();
+	});
+
+	it('should return html and a status of 200', function() {
+		return chai.request(app)
+		  	.get('/')
+		  	.then(function(res) {
+		   		expect(res).to.have.status(200);    // <= Test completes before this runs
+	  	});		
+	});	
 });
 	
