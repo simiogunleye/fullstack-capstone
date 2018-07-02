5d5ca6fce09a98f688249dc1a62cbfef7f65a223
simiogunleye <cimi@Simis-MacBook-Pro.local>
2018-07-02 07:42:17 -0700

    added weekly calender, needs more styling

--- public/dashboard.js
+++ public/dashboard.js
@@ -1,3 +1,8 @@
+var today = new Date();
+var date = (today.getMonth()+1)+'/'+today.getDate()+'/'+today.getFullYear();
+document.getElementById("date").innerHTML = date;
+
+
 function selectBodyPartScreen() {
 	return `
 	        <section id="body-parts" role="region">
@@ -18,8 +23,49 @@ function dashboardScreen() {
 	`
 }
 
+function calendarView() {
+	return `
+	<div class="weekOf">July</div>
+    <button class="accordion">Sunday</button>
+      <div class="panel">
+        <p>Workouts added...</p>
+      </div>
+
+    <button class="accordion">Monday</button>
+      <div class="panel">
+        <p>Workouts added...</p>
+      </div>
+
+    <button class="accordion">Tuesday</button>
+      <div class="panel">
+        <p>Workouts added...</p>
+      </div>
+
+    <button class="accordion">Wednesday</button>
+      <div class="panel">
+        <p>Workouts added...</p>
+      </div>
+
+    <button class="accordion">Thursday</button>
+      <div class="panel">
+        <p>Workouts added...</p>
+      </div>
+
+    <button class="accordion">Friday</button>
+      <div class="panel">
+        <p>Workouts added...</p>
+      </div>
+
+    <button class="accordion">Saturday</button>
+      <div class="panel">
+        <p>Workouts added...</p>
+      </div>
+	`
+}
+
 function chooseWorkout() {
 	console.log('working2');
+
 	$('.dashboard-container').on('click', '.choose-btn', function(event){
 		selectBodyPart();
 	});
@@ -39,15 +85,44 @@ function selectBodyPart() {
 
 function showWorkoutOptions() {
 	$('.dashboard-container').show();
+	$('.banner-pic').hide();
+	$('#date').show();
+	//work here
+	//$('#calendar').html(calendarView());
+	calendar();
 	$('.dashboard-container').html(dashboardScreen());
+}
+
+function calendar() {
+	let acc = document.getElementsByClassName("accordion");
+	let i;
+
+	for (i = 0; i < acc.length; i++) {
+	    acc[i].addEventListener("click", function() {
+	        /* Toggle between adding and removing the "active" class,
+	        to highlight the button that controls the panel */
+	        this.classList.toggle("active");
+
+	        /* Toggle between hiding and showing the active panel */
+	        let panel = this.nextElementSibling;
+	        if (panel.style.display === "block") {
+	            panel.style.display = "none";
+	        } else {
+	            panel.style.display = "block";
+	        }
+	    });
+}
+$('#calendar').html(calendarView());
+
 }
 
 function hideForm() {
 	$('.form-container').hide();
 }
 
-
 function signUp() {
+	$('.banner-pic').show();
+	$('#date').hide();
 	$('.signup-form').on('submit', function(event){
 		event.preventDefault();
 		hideForm();
@@ -57,15 +132,11 @@ function signUp() {
 }
 
 
-
-
-
 function dashboardStart() {
 	signUp();
 	selectBodyPart();
 	chooseWorkout();
 	createWorkout();
 }
-
 
 $(dashboardStart());
