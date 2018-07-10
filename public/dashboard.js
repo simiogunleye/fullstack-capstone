86d38d7a71d677c950cd61d7d58d09ef358c76f5
simiogunleye <cimi@Simis-MacBook-Pro.local>
2018-07-10 14:13:05 -0700

    all views implemented

--- public/dashboard.js
+++ public/dashboard.js
@@ -10,6 +10,168 @@ let date = firstDay + '-' + lastDay;
 
 
 
+
+
+function selectBodyPartScreen() {
+	return `
+		<div class="instructions">
+			<p>
+				You can now create your own workout. 
+			    Select which body part you want to find exercises for, 
+			    choose your exercises, then create a name and save your workout. 
+			</p>
+		</div>
+		<div class="workout-name-form-container">  
+          <form class= "workout-name-form">
+            <fieldset class="workout">
+              <label for="workout-name" required></label>
+              <input type="text" name="workout-name" class="workout-name" placeholder="Name your workout...">
+              <button class="save-btn" type="submit">Save</button>
+            </fieldset>    
+          </form>
+        </div> 	
+		<div class="bodyPartsContainer">
+	        <section id="body-parts" role="region">
+	          <button class="arms-bodyPart-btn">Arms</button>
+	          <button class="core-bodyPart-btn">Core</button>
+	          <button class="glutes-bodyPart-btn">Glutes</button>
+	          <button class="legs-bodyPart-btn">Legs</button>
+	          <button class="save-workout-btn" type="submit">Save Workout</button>
+	        </section>
+        </div>
+	`
+}
+
+function selectBodyPart() {
+	$('.dashboard-container').html(selectBodyPartScreen());
+}
+
+
+function armsScreen() {
+	return `
+		<div class="exerciseScreenContainer">
+			<header>
+				<h1>Arms</h1>
+			</header>
+			<section "choose-exercises">
+				<button class="exercise-choice"><a href="#" class="plus">+</a>Exercise 1</button>
+			</section>
+			<div class="backto-bodyPart"><a href="#">Choose another body part</a></div>	
+		</div>
+	`
+}
+
+function coreScreen() {
+	return `
+		<div class="exerciseScreenContainer">
+			<header>
+				<h1>Core</h1>
+			</header>
+			<section "choose-exercises">
+				<button class="exercise-choice"><a href="#" class="plus">+</a>Exercise 1</button>
+			</section>
+			<div class="backto-bodyPart"><a href="#">Choose another body part</a></div>	
+		</div>
+	`
+}
+
+function legsScreen() {
+	return `
+		<div class="exerciseScreenContainer">
+			<header>
+				<h1>Legs</h1>
+			</header>
+			<section "choose-exercises">
+				<button class="exercise-choice"><a href="#" class="plus">+</a>Exercise 1</button>
+			</section>
+			<div class="backto-bodyPart"><a href="#">Choose another body part</a></div>	
+		</div>
+	`
+}
+
+function glutesScreen() {
+	return `
+		<div class="exerciseScreenContainer">
+			<header>
+				<h1>Glutes</h1>
+			</header>
+			<section "choose-exercises">
+				<button class="exercise-choice"><a href="#" class="plus">+</a>Exercise 1</button>
+			</section>
+			<div class="backto-bodyPart"><a href="#">Choose another body part</a></div>	
+		</div>
+	`
+}
+
+
+function bodyPartExercises() {
+	$('.dashboard-container').on('click', '.arms-bodyPart-btn', function(event) {
+		console.log('arm button clicked');
+		$('.dashboard-container').html(armsScreen());
+	});
+
+	$('.dashboard-container').on('click', '.core-bodyPart-btn', function(event) {
+		console.log('core button clicked');
+		$('.dashboard-container').html(coreScreen());
+	});
+
+	$('.dashboard-container').on('click', '.glutes-bodyPart-btn', function(event) {
+		console.log('glutes button clicked');
+		$('.dashboard-container').html(glutesScreen());
+	});
+
+	$('.dashboard-container').on('click', '.legs-bodyPart-btn', function(event) {
+		console.log('legs button clicked');
+		$('.dashboard-container').html(legsScreen());
+	});
+}
+
+function chooseWorkoutScreen() {
+	return `
+		<div class="workout-choices">
+			<h2>Choose your workout</h2><br>
+			<h3><a href="#" class=workout-choice-name>Total Body</a></h3>
+				<ul>
+					<span><li>exercise</li></span>
+					<span><li>exercise</li></span>
+					<span><li>exercise</li></span>
+					<span><li>exercise</li></span>	
+				</ul>
+			<h3><a href="#" class=workout-choice-name>Muscle Building</a></h3>
+				<ul>
+					<span><li>exercise</li></span>
+					<span><li>exercise</li></span>
+					<span><li>exercise</li></span>
+					<span><li>exercise</li></span>	
+				</ul>
+			<h3><a href="#" class=workout-choice-name>High Intensity Interval Training</a></h3>
+				<ul>
+					<span><li>exercise</li></span>
+					<span><li>exercise</li></span>
+					<span><li>exercise</li></span>
+					<span><li>exercise</li></span>
+				</ul>
+		</div>
+
+	`
+}
+
+function chooseWorkout() {
+	$('.dashboard-container').on('click', '.choose-btn', function(event){
+		hideCalendar();
+		$('.dashboard-container').html(chooseWorkoutScreen());	
+	});
+} 
+
+
+function createWorkout() {
+	$('.dashboard-container').on('click', '.create-btn', function(event){
+		hideCalendar();
+		selectBodyPart();
+	});
+}
+
+
 function calendarView() {
 	return `
 	<div class="weekOf">${date}</div>
@@ -50,74 +212,6 @@ function calendarView() {
 	`
 }
 
-function chooseWorkout() {
-	$('.dashboard-container').on('click', '.choose-btn', function(event){
-		hideCalendar();
-		//Create page/function to show at least 3 exercise regimens, delete 
-		//selectBodyPart() function  
-		selectBodyPart();
-	});
-} 
-
-// function createInstructions() {
-// 	return `
-// 	<div class="instructions">
-// 		<p>You can now create your own workout. <br>
-// 		   First, select which body part you want to find exercises for one at a time,<br>
-// 		   choose your exercises then create a name and save your workout. 
-// 		</p>
-// 	</div>	
-// 	`
-// }
-
-function hideCalendar() {
-	$('#calendar').hide();
-}
-
-
-function selectBodyPartScreen() {
-	return `
-		<div class="instructions">
-			<p>
-				You can now create your own workout. 
-			    Select which body part you want to find exercises for, 
-			    choose your exercises, then create a name and save your workout. 
-			</p>
-		</div>
-		<div class="workout-name-form-container">  
-          <form class= "workout-name-form">
-            <fieldset class="workout">
-              <label for="workout-name" required></label>
-              <input type="text" name="workout-name" class="workout-name" placeholder="Name your workout...">
-              <button class="save-btn" type="submit">Save</button>
-            </fieldset>    
-          </form>
-        </div> 	
-		<div>
-	        <section id="body-parts" role="region">
-	          <button class="bodyPart-btn">Arms</button>
-	          <button class="bodyPart-btn">Core</button>
-	          <button class="bodyPart-btn">Glutes</button>
-	          <button class="bodyPart-btn">Legs</button>
-	        </section>
-        </div>
-	`
-}
-
-function selectBodyPart() {
-	//$('.dashboard-container').html(createInstructions());
-	$('.dashboard-container').html(selectBodyPartScreen());
-}
-
-
-function createWorkout() {
-	$('.dashboard-container').on('click', '.create-btn', function(event){
-		hideCalendar();
-		selectBodyPart();
-		//$('.dashboard-container').html(createInstructions());
-	});
-}
-
 
 function calendar() {
 	$('body').on('click', '.accordion', function(event){ 
@@ -126,7 +220,9 @@ function calendar() {
     $('#calendar').html(calendarView());
 }
 
-
+function hideCalendar() {
+	$('#calendar').hide();
+}
 
 
 function dashboardScreen() {
@@ -138,13 +234,6 @@ function dashboardScreen() {
 	`
 }
 
-function showWorkoutOptions() {
-	addWorkoutsToggle();
-	$('.banner-pic').hide();
-	$('#date').show();
-	calendar();
-	$('.dashboard-container').html(dashboardScreen());
-}
 
 
 function addWorkoutsToggle() {
@@ -153,6 +242,13 @@ function addWorkoutsToggle() {
 	});
 }
 
+function showWorkoutOptions() {
+	addWorkoutsToggle();
+	$('.banner-pic').hide();
+	$('#date').show();
+	calendar();
+	$('.dashboard-container').html(dashboardScreen());
+}
 
 function hideForm() {
 	$('.form-container').hide();
@@ -175,6 +271,7 @@ function dashboardStart() {
 	selectBodyPart();
 	chooseWorkout();
 	createWorkout();
+	bodyPartExercises();
 }
 
 $(dashboardStart());
