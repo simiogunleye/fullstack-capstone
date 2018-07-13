961e69ed5fce538f21f7285e2923f04ae65825ba
simiogunleye <cimi@Simis-MacBook-Pro.local>
2018-07-13 16:21:25 -0700

    done with client

--- public/dashboard.js
+++ public/dashboard.js
@@ -284,7 +284,7 @@ let date = firstDay + '-' + lastDay;
 			<h3>${workoutType}</h3>
 			<section class="choose-exercises-container" role="region">
 			</section>
-			<a href="#">Choose another body part</a>
+			<a class="back-to-body" href="#">Choose another body part</a>
 		</div>`;
 }
 
